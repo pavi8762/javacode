f66f1c77e5888623651ad38bcc1ecc34ce144e0d
Pavithra <vachala.p@gmail.com>
2020-12-11 15:05:52 +0000

    With Jenkin and Maven dependency

--- RedBusProject/src/tests/ListenerClass.java
+++ RedBusProject/src/tests/ListenerClass.java
@@ -10,6 +10,43 @@ public class ListenerClass implements ITestListener{
 		
 		System.out.println(Result.getName()+ " case finished");
 	}
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		System.out.println(context.getName()+ " case Started");
+		
+	}
 	
 
 
